98d79a8bf930ea6a54dfaecfee83387d52193898
71Arid <brian87noel@gmail.com>
2022-12-09 05:49:55 -0800

    i

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - digit numbers from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

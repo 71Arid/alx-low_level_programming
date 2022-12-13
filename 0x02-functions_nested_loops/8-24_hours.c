a724123b0079a957428f323265a68021d603af01
71Arid <brian87noel@gmail.com>
2022-12-13 09:59:29 -0800

    l

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,14 +9,14 @@ void jack_bauer(void)
 
 	for (hrs = 0; hrs <= 23; hrs++)
 	{
-		for (min = 0; min <= 59; min++)
+		for (mins = 0; mins <= 59; mins++)
 		{
 			_putchar((hrs / 10) + '0');
 			_putchar((hrs % 10) + '0');
 			_putchar(':');
 			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
-			-putchar('\n');
+			_putchar('\n');
 		}
 	}
 }

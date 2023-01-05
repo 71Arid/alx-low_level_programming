90db53bd11948380125594866cca737e3f46742b
71Arid <brian87noel@gmail.com>
2023-01-05 07:45:09 -0800

    l

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,11 +3,12 @@
  * _strpbrk - Searches a string for any of a set of bytes
  * @s: The string to be searched.
  * @accept: The set of bytes to be searched for.
- * Return: If a set is matched - a pointer to the matched byte. If no set is matched - NULL.
+ * Return: If a set is matched - a pointer to the matched
  */
 char *_strpbrk(char *s, char *accept)
 {
 	int index;
+
 	while (*s)
 	{
 		for (index = 0; accept[index]; index++)

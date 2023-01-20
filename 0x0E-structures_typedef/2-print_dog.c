79bf528628249f17375f6e6e8e52ecb2b9c5e730
71Arid <brian87noel@gmail.com>
2023-01-20 11:53:36 +0300

    float corrected

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,17 +11,17 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name != NULL)
-		printf("Name: %s\n", p->name);
+		printf("Name: %s\n", d->name);
 	else
 		printf("Name: (nil)\n");
 
-	if (d->age != NULL)
-		printf("Age: %d\n", p->age);
+	if (d->age > 0)
+		printf("Age: %f\n", d->age);
 	else
 		printf("Age: (nil)\n");
 
 	if (d->owner != NULL)
-		printf("Owner: %s\n", p->owner);
+		printf("Owner: %s\n", d->owner);
 	else
 		printf("Owner: (nil)\n");
 }

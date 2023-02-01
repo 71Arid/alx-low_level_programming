c4b1e36331974b16de9f846b03db7fb027c3cc83
71Arid <brian87noel@gmail.com>
2023-02-01 20:22:33 +0300

    new

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,6 +14,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
+		free(newNode);
 		return (NULL);
 	newNode->str = strdup(str);
 

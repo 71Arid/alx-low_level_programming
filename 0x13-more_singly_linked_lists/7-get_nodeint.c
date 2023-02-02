ba749c1ba470e61e8c5ced16d6ede653f0702a6f
71Arid <brian87noel@gmail.com>
2023-02-02 20:30:42 +0300

    new

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,7 +8,7 @@
   */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current;
+	listint_t *current = head;
 	unsigned int count = 0;
 
 	while (current != NULL)

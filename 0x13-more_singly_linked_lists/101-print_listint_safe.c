7c52efe0692735b2df119458247d83978c267f23
71Arid <brian87noel@gmail.com>
2023-02-09 11:39:31 +0300

    printsafe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,31 +2,65 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
+  * free_listp - frees list
+  * @head: head of list
+  * Return: no return
+  */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *c;
+
+	if (head != NULL)
+	{
+		c = *head;
+		while ((temp = c) != NULL)
+		{
+			c = c->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
  *print_listint_safe - print listint_t
  *@head: pointer to a const struct
  *Return: number of nodes
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current, *check;
 	size_t count = 0;
+	listp_t *hp, *n, *add;
 
-	current = head;
-	while (current)
+	hp = NULL;
+	while (head != NULL)
 	{
-		check = head;
-		count++;
-		printf("[%p] %d\n", (void *)current, current->n);
-		current = current->next;
+		n = malloc(sizeof(listp_t));
 
-		while (check != current && check)
-			check = check->next;
+		if (n == NULL)
+			exit(98);
+		n->p = (void *)head;
+		n->next = hp;
+		hp = n;
 
-		if (check == current)
+		add = hp;
+
+		while (add->next != NULL)
 		{
-			printf("->[%p] %d\n", (void *)current, current->n);
-			break;
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hp);
+				return (count);
+			}
 		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		count++;
 	}
+	free_listp(&hp);
 	return (count);
 }
+

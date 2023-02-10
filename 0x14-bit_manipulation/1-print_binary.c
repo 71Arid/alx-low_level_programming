dd6f308009135841252a019c05c2e380bade8c50
71Arid <brian87noel@gmail.com>
2023-02-10 09:55:47 +0300

    n > 0

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 /**
   * print_binary - prints binary
   * @n: integer
   */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n > 0)
 	{
 		print_binary(n >> 1);
 		_putchar((n & 1) + '0');

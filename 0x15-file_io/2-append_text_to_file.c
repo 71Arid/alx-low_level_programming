b68e1d1306e3d97ac9db0cc6756285924dbb7907
71Arid <brian87noel@gmail.com>
2023-02-16 21:18:52 +0300

    corrections

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -43,7 +43,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!text_content)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 
 	buff = _strlen(text_content);
@@ -52,7 +52,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (a == -1)
 	{
 		close(fd);
-		return(-1);
+		return (-1);
 	}
 	close(fd);
 	return (1);

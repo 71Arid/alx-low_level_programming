825c66f0ef0b501a5c3bf255e8bf79013830b37e
71Arid <brian87noel@gmail.com>
2023-02-16 19:43:52 +0300

    jj

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,9 +23,9 @@ int main(int argc, char **argv)
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to == -1)
 		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
-	from = open(argv[1], O_RONLY);
+	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-		dprint(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
+		dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
 	while (1)
 	{
 		r = read(from, buffer, 1024);
@@ -47,7 +47,6 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
 	on_close = close(to);
 	if (on_close == -1)
-		dprintf(STRERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
 	return (0);
 }
-

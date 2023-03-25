18e3be243fe2b5de70f44f3ad116ee877446ad6e
71Arid <brian87noel@gmail.com>
2023-03-25 09:28:16 +0300

    addbeg

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *current = *head;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return NULL;
+
+	new->prev = NULL;
+	new->next = current;
+	new->n = n;
+
+	if (current != NULL)
+		current->prev = new;
+
+	*head = new;
+	return new;
+}

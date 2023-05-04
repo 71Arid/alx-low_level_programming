dc3668f9dc03169c0d532e8cf457f987a45a81ec
71Arid <brian87noel@gmail.com>
2023-05-04 07:04:47 -0700

    print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints tables
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *bucket;
+	int fin = 0;
+
+	if (!ht)
+		return;
+
+	print("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		bucket = ht->array[i];
+		while (bucket)
+		{
+			if (fin)
+				print(", ");
+			printf("'%s': '%s'", bucket->key, bucket->value);
+			fin = 1;
+			bucket = bucket->next;
+		}
+	}
+	print("}\n");
+}

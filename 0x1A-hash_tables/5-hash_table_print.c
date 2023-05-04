5068b6936f1aff907d145427df8589bde329749f
71Arid <brian87noel@gmail.com>
2023-05-04 07:07:08 -0700

    print

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -14,7 +14,7 @@ void hash_table_print(const hash_table_t *ht)
 	if (!ht)
 		return;
 
-	print("{");
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		bucket = ht->array[i];
@@ -27,5 +27,5 @@ void hash_table_print(const hash_table_t *ht)
 			bucket = bucket->next;
 		}
 	}
-	print("}\n");
+	printf("}\n");
 }
